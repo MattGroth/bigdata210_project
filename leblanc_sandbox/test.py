582a20b11826d32f4ff545899a36b0e7e6090e38
hecate007 <hecate007@sandbox.hortonworks.com>
2016-11-02 20:27:13 +0000

    updated test.py

---
+++ leblanc_sandbox/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from pyspark import SparkConf, SparkContext
+import collections
+
+
+dropoutRateCsvPath = @""
+enrollmentCsvPath = @""
+schoolDistPopCsvPath = @""
+
+print "done"
