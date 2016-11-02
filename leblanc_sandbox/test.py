9e59af9c61d16be76105b595c302ed92e9c3252f
hecate007 <leblan7@gmail.com>
2016-11-02 21:01:06 +0000

    updated test.py

--- leblanc_sandbox/test.py
+++ leblanc_sandbox/test.py
@@ -6,10 +6,11 @@ import os
 import collections
 
 
-dropoutRateCsvPath = @"../data/High_School_Dropout_Statistics_by_County_2012-2013_School_Year_5-Year_Cohort_Dropout_Rates.csv"
-enrollmentCsvPath = @"../data/K-12_Public_School_Enrollment_by_Grade_Level_October_Enrollment_Report_2009-2013.csv"
-schoolDistPopCsvPath = @"../data/WAOFM_-_SAEP_-_School_District_Population_Estimates__2000-2016.csv"
+dropoutRateCsvPath = r"../data/High_School_Dropout_Statistics_by_County_2012-2013_School_Year_5-Year_Cohort_Dropout_Rates.csv"
+enrollmentCsvPath = r"../data/K-12_Public_School_Enrollment_by_Grade_Level_October_Enrollment_Report_2009-2013.csv"
+schoolDistrictPopCsvPath = r"../data/WAOFM_-_SAEP_-_School_District_Population_Estimates__2000-2016.csv"
 
-print "path is valid: {0}".format(os.path.exists(dropoutRateCsvPath))
+#load school district population data
+df = SparkContext
 
 print "done"
